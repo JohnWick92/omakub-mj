1c22b051c1e2905ff5e929a64873d7fdfecd8f6d
JohnWick92 <grumec92@gmail.com>
2024-11-06 20:59:17 -0300

    Fixing zellij

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,5 +1,5 @@
 yay -S --needed zellij --noconfirm
 
 mkdir -p ~/.config/zellij/themes
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/share/omakub/configs/zellij.kdl ~/.config/zellij/config.kdl
+[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp -r ~/.local/share/omakub/configs/zellij ~/.config/
 cp ~/.local/share/omakub/themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
