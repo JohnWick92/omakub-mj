584e1903ac71391b88e682fd5612f3e64f348d3a
JohnWick92 <grumec92@gmail.com>
2024-11-06 20:39:11 -0300

    Fixing more bugs

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,3 +1,3 @@
-yay -S --needed fzf ripgrep bat eza du-dust zoxide plocate btop fd tldr --noconfirm
+yay -S --needed fzf ripgrep bat eza dust zoxide plocate btop fd tldr --noconfirm
 
 # the Ubuntu version had apache2-utils, which could be apache-tools in Arch, but not sure if it's necessary TODO
