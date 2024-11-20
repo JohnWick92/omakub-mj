86218a4c30de844e7085c9c58088b2fe518b0f28
shanks <grumec92@gmail.com>
2024-11-20 13:54:18 -0300

    Fixing some config files

--- install/terminal/atuin.sh
+++ install/terminal/atuin.sh
@@ -1,4 +1,5 @@
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+cp ~/.atuin/bin/env.fish ~/.cargo/
 
 if $(gum confirm "Press (Y) to register a new account into Atuin or (N) if want to log back in."); then
   atuin register
